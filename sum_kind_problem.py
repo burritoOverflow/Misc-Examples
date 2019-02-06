8735f62f39e5dc3e222613750e26dfcb0982588d
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2019-02-06 03:04:45 -0500

    Update sum_kind_problem.py

--- sum_kind_problem.py
+++ sum_kind_problem.py
@@ -1,4 +1,4 @@
-# solution to open.kattis sum kind of problem
+# python3 solution to open.kattis sum kind of problem
 
 p = int(input())
 
@@ -6,7 +6,4 @@ for i in range(0, p):
     k, n = input().split()
     k = int(k)
     n = int(n)
-    s1 = n * (n + 1) // 2
-    s2 = n ** 2
-    s3 = n * (n + 1)
-    print(str(k), str(s1), str(s2), str(s3))
+    print(str(k), str(n * (n + 1) // 2), str(n ** 2), str(n * (n + 1)))
