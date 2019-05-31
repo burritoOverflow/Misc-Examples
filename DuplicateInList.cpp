a5ad9dbfdb15ea7c63a48d6d53627e3bc68f2bf5
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2019-05-31 01:17:53 -0400

    Add files via upload

---
+++ DuplicateInList.cpp
@@ -0,0 +1,39 @@
+#include <set>
+#include <iostream>
+#include <assert.h>
+
+int Duplicate(int arr[], int size)
+{
+  std::set<int> myset;
+  // if -1, no duplicates
+  int dup = -1;
+  
+  // used for the return value
+  std::pair<std::set<int>::iterator,bool> ret;
+  
+  for(int i = 0; i < size; i++)
+  {
+    if (myset.insert(arr[i]).second == false)
+    {
+      dup = arr[i];
+    }
+  }
+  return dup;
+}
+
+int main() {
+  
+  int list[] = { 2, 1, 4, 3, 5, 12, 43, 12, 56};
+  int list2[] = { 12, 45, 43, 89, 90, 90, 14};
+  int list3[] = {1};
+  
+  int size = sizeof(list) / sizeof(*list);
+  int size2 = sizeof(list2) / sizeof(*list2);
+  int size3 = sizeof(list3) / sizeof(*list3);
+  
+  assert(Duplicate(list, size) == 12);
+  assert(Duplicate(list2, size2) == 90);
+  assert(Duplicate(list3, size3) == -1);
+
+
+}
