c23097a5278ef3db4c8c93f9f1ef3660d1057a30
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2019-08-22 19:24:57 -0400

    Update py_file_read_write.py
    Notes

--- LinuxSystemProgramming/fileIO/py_file_read_write.py
+++ LinuxSystemProgramming/fileIO/py_file_read_write.py
@@ -1,5 +1,7 @@
 # note the more 'pythonic' way of performing the same 
-# operation as the previous example
+# operation as the previous example. These operations return
+# a file object. Also, the 'with' keyword here sets up a try-catch-finally
+# operation behind the scenes, so there is no need to close the files manually.
 
 with open("foo", "rb") as fin:
     with open("bar", "wb") as fout:
