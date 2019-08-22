6b3dfcc61b1169e20a4a643a9e6ba0f5e9a2196c
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2019-08-22 09:04:47 -0400

    Update sum_digits_recursive.py
    added doctests

--- sum_digits_recursive.py
+++ sum_digits_recursive.py
@@ -1,12 +1,24 @@
 ''' Sum Digits without using a while statement '''
 
 def split(n):
-    ''' split positive n into all but its last digit and its last digit '''
+    '''
+    split positive n into all but its last digit and its last digit
+    >>> split(1043)
+    (104, 3)
+    >>> split(134)
+    (13, 4)
+    '''
     return n // 10, n % 10
 
 
 def sum_digits(n):
-    ''' return the sum of the digits of the positive integer n '''
+    '''
+    return the sum of the digits of the positive integer n
+    >>> sum_digits(1043)
+    8
+    >>> sum_digits(1414)
+    10 
+    '''
     if n < 10:
         return n
     else:
