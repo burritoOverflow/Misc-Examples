334a3ea098223ecdbcf646c832ca730a7a4c7fce
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2019-08-22 18:53:42 -0400

    Create rawIO.c

---
+++ LinuxSystemProgramming/fileIO/rawIO.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  char *buffer;
+  int fd, size, count, i;
+
+  if (argc != 3) {
+    printf("Usage: %s blocksize blockcount\n", argv[0]);
+    exit(1);
+  }
+
+  fd = open("rawio.out", O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+  // size of the blocks to write, the number of blocks to write
+  size = atoi(argv[1]);
+  count = atoi(argv[2]);
+
+  buffer = malloc(size);
+
+  for (i = 0; i < count; i++) {
+    write(fd, buffer, size);
+  }
+
+  close(fd);
+
+}
