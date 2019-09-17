ee79ba1dc01e97dcd2a40907184a62804df5571e
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2019-09-17 17:37:14 -0400

    Add files via upload

---
+++ recursiveLinearSeach.js
@@ -0,0 +1,19 @@
+const assert = require('assert');
+
+recursiveLinearSearch = (arr, n, val) => {
+  if (n < 0) {
+    // value not found
+    return false
+  }
+  if (arr[n] === val) {
+    return true
+  }
+  return recursiveLinearSearch(arr, n - 1, val)
+}
+
+
+const arr = [12, 13, 67, 78, 891, 90, 41, 54];
+
+arr.forEach((value) => {
+  assert(recursiveLinearSearch(arr, arr.length, value));
+});
