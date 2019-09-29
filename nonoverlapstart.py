a311a5d077da5683baecdbc1200da5b6a427c422
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2019-09-29 16:32:12 -0400

    Update nonoverlapstart.py
    reconsideration of a possible edge case where two intervals have same start time

--- nonoverlapstart.py
+++ nonoverlapstart.py
@@ -12,7 +12,7 @@ def find_non_overlap_start(interval_list):
 	# sort list by interval start time (index 0)
 	interval_list = sorted(interval_list, key=lambda k: k[0])
 	for interval in interval_list:
-		if interval[0] > prev_start_time and interval[0] < prev_end_time:
+		if interval[0] >= prev_start_time and interval[0] < prev_end_time:
 			# overlaps
 			print('Overlap {}'.format(interval[0]))
 		else:
@@ -22,6 +22,6 @@ def find_non_overlap_start(interval_list):
 		prev_end_time = interval[1]
 	return (count, valid_start_times)
 
-interval_list = [(1, 3), (2, 4), (9, 12), (5, 7), (3, 7)]
+interval_list = [(5, 9), (1, 3), (2, 4), (9, 12), (5, 7), (3, 7)]
 
 print(find_non_overlap_start(interval_list))
