e53bf53a4f4e6015d50702fa44539a76bb357e73
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2019-10-18 00:50:30 -0400

    Update closestDistanceArr.js

--- closestDistanceArr.js
+++ closestDistanceArr.js
@@ -1,10 +1,16 @@
 const values = [12, 41, 90, 93, 2, 4, 8, 21, 54];
-values.sort();
+
+let sortNumber = (a, b) => {
+	return a - b;
+}
+
+values.sort(sortNumber);
 
 let min = Infinity;
 
 for(let i = 0; i < values.length - 1; i++) {
-	min = Math.min(min, Math.abs(values[i+1] - values[i]));
+	let difference = Math.abs(values[i + 1] - values[i]);
+	min = Math.min(min, difference);
 }
 
 console.log(min);
