1bf860bf71ed89d6b5b5d0f7fc4e0a052443c645
burritoOverflow <47379530+burritoOverflow@users.noreply.github.com>
2020-02-17 16:18:00 -0500

    Create quickMirrorS3.sh

---
+++ quickMirrorS3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    echo "Usage: ./mirrorSite.sh <url> <bucket-name>"
+    exit 1
+fi
+
+# change user-agent as one sees fit
+wget -e robots=off --mirror --user-agent="Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.40607)"  \
+--convert-links --adjust-extension --page-requisites --no-parent $1
+
+dirname=`echo $1 | awk -F[/:] '{print $4}'`
+
+cd $dirname
+
+aws s3 mb s3://$2
+aws s3 website s3://$2/ --index-document index.html --error-document index.html
+aws s3 cp . s3://$2 --recursive --acl public-read
+
+echo "Removing $dirname"
+cd ..
+rm -rf $dirname
